55c618fe5a110bd45c5ff2319a210292dea5fd62
Yesika Perez Ravelo <yesikapr@gmail.com>
2023-06-08 16:51:27 -0300

    calendario

--- js/appointment.js
+++ js/appointment.js
@@ -5,8 +5,10 @@ let events = localStorage.getItem("events")
   : [];
 
 const calendar = document.getElementById("calendar");
-const newEventModal = document.getElementById("NewEventModal");
-const backDrop = document.getElementById("ModalBackDrop");
+const newEventModal = document.getElementById("newEventModal");
+const deleteEventModal = document.getElementById("deleteEventModal");
+const backDrop = document.getElementById("modalBackDrop");
+const eventTitleInput = document.getElementById("eventTitleInput");
 const weekdays = [
   "Domingo",
   "Lunes",
@@ -23,7 +25,8 @@ function openModal(date) {
   const eventForDay = events.find((e) => e.date === clicked);
 
   if (eventForDay) {
-    console.log("Ya esta agendado");
+    document.getElementById("eventText").innerText = eventForDay.title;
+    deleteEventModal.style.display = "block";
   } else {
     newEventModal.style.display = "block";
   }
@@ -44,36 +47,82 @@ function load() {
 
   const firstDayOfMonth = new Date(year, month, 1);
   const daysInMonth = new Date(year, month + 1, 0).getDate();
+
   const dateString = firstDayOfMonth.toLocaleDateString("es-us", {
     weekday: "long",
     year: "numeric",
     month: "numeric",
     day: "numeric",
   });
+  const paddingDays = weekdays.indexOf(dateString.split(", ")[0]);
 
-  const paddingDays = weekdays.indexOf(dateString.split(",")[0]);
-
-  document.getElementById("monthDisplay").innerText = ` ${dt.toLocaleDateString(
+  document.getElementById("monthDisplay").innerText = `${dt.toLocaleDateString(
     "es-us",
     { month: "long" }
   )} ${year}`;
 
   calendar.innerHTML = "";
 
-  for (let i = 0; i <= paddingDays + daysInMonth; i++) {
+  for (let i = 1; i <= paddingDays + daysInMonth; i++) {
     const daySquare = document.createElement("div");
     daySquare.classList.add("day");
 
+    const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
     if (i > paddingDays) {
       daySquare.innerText = i - paddingDays;
+      const eventForDay = events.find((e) => e.date === dayString);
 
-      daySquare.addEventListener("click", () => console.log("click"));
+      if (i - paddingDays === day && nav === 0) {
+        daySquare.id = "currentDay";
+      }
+
+      if (eventForDay) {
+        const eventDiv = document.createElement("div");
+        eventDiv.classList.add("event");
+        eventDiv.innerText = eventForDay.title;
+        daySquare.appendChild(eventDiv);
+      }
+
+      daySquare.addEventListener("click", () => openModal(dayString));
     } else {
       daySquare.classList.add("padding");
     }
 
     calendar.appendChild(daySquare);
   }
+}
+
+function closeModal() {
+  eventTitleInput.classList.remove("error");
+  newEventModal.style.display = "none";
+  deleteEventModal.style.display = "none";
+  backDrop.style.display = "none";
+  eventTitleInput.value = "";
+  clicked = null;
+  load();
+}
+
+function saveEvent() {
+  if (eventTitleInput.value) {
+    eventTitleInput.classList.remove("error");
+
+    events.push({
+      date: clicked,
+      title: eventTitleInput.value,
+    });
+
+    localStorage.setItem("events", JSON.stringify(events));
+    closeModal();
+  } else {
+    eventTitleInput.classList.add("error");
+  }
+}
+
+function deleteEvent() {
+  events = events.filter((e) => e.date !== clicked);
+  localStorage.setItem("events", JSON.stringify(events));
+  closeModal();
 }
 
 function initButtons() {
@@ -86,6 +135,13 @@ function initButtons() {
     nav--;
     load();
   });
+
+  document.getElementById("saveButton").addEventListener("click", saveEvent);
+  document.getElementById("cancelButton").addEventListener("click", closeModal);
+  document
+    .getElementById("deleteButton")
+    .addEventListener("click", deleteEvent);
+  document.getElementById("closeButton").addEventListener("click", closeModal);
 }
 
 initButtons();
